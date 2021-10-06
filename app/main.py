3960b8e18d43e839383509cfb42f8ff5a48eca20
GiDaeHyeon <spliter2157@gmail.com>
2021-10-06 17:29:26 +0900

    로드밸런싱 기본  세팅

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-import osfrom fastapi import FastAPI, Request, Queryfrom model import SentimentCLFapp = FastAPI()model = SentimentCLF(ckpt_dir=os.environ['CKPT_DIR'])@app.get('/')async def sentiment_clf(request: Request,                        text: Query(None)):    sentiment = model.sentiment_clf(text)    return {'sentiment': sentiment}
+import os
+from fastapi import FastAPI, Request, Query
+from .model import SentimentCLF
+
+
+app = FastAPI()
+model = SentimentCLF(ckpt_dir=os.environ['CKPT_DIR'])
+
+
+@app.post('/sentiment')
+async def sentiment_clf(request: Request) -> dict:
+    req = await request.json()
+    text = req['text']
+    sentiment = model.sentiment_clf(text)
+    return {'sentiment': sentiment}
--- app/model.py
+++ app/model.py
@@ -1,1 +1,23 @@
-import torchfrom typing import Optionalfrom NSMC.datamodule import BERTDatasetfrom NSMC.trainmodule import Classifierclass SentimentCLF:    def __init__(self,                 ckpt_dir=None):        # Model        self.model = Classifier().load_from_checkpoint(ckpt_dir)        self.model.eval()        # Tokenizer        self.tokenizer = BERTDataset().tokenize    def sentiment_clf(self, x: Optional[str]) -> Optional[int]:        input_ids, attention_mask, token_type_ids = self.tokenizer(x)        y_hat = self.model(input_ids[None], attention_mask[None], token_type_ids[None])        return int(torch.argmax(y_hat[0]).item())
+import torch
+from typing import Optional
+
+from NSMC.datamodule import BERTDataset
+from NSMC.trainmodule import Classifier
+
+
+class SentimentCLF:
+    def __init__(self,
+                 ckpt_dir=None):
+        # Model
+        model = Classifier()
+        self.model = model.load_from_checkpoint(ckpt_dir)
+        self.model.eval()
+
+        # Tokenizer
+        self.tokenizer = BERTDataset(use_=False).tokenize
+
+    def sentiment_clf(self, x: Optional[str]) -> Optional[int]:
+        input_ids, attention_mask, token_type_ids = self.tokenizer(x)
+        y_hat = self.model(input_ids[None], attention_mask[None], token_type_ids[None])
+        return int(torch.argmax(y_hat[0]).item())
+
